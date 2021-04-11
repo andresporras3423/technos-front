821345afec851e2825b8b4d7dd46b2205903a5c0
oscarporras02 <oscarrussi@outlook.com>
2021-04-10 20:32:00 -0500

    add routes for login and signup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
 import './App.css';
+import {
+  Route, Switch,
+} from 'react-router-dom';
+import Login from './Login';
+import Main from './Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        hello world
-      </header>
-    </div>
+    <Switch>
+    <Route exact path="/login">
+    <Login message="this is login" />
+    </Route>
+    <Route exact path="/signup">
+    <Login message="this is signup" />
+    </Route>
+    <Route exact path="/">
+    <Main />
+    </Route>
+    </Switch>
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,10 @@
+function Login(props) {
+    const {message} = props;
+  return (
+    <div>
+     {message}
+    </div>
+  );
+}
+
+export default Login;
