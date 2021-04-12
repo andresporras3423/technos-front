94a00b39710ab06abf72571d4b01c975cb37a4ae
oscarporras02 <oscarrussi@outlook.com>
2021-04-12 16:24:58 -0500

    add stylying for navbar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,9 @@
+import Navbar from './Navbar';
+
 function Dashboard() {
   return (
     <div>
-     this is main
+     <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+function Navbar() {
+  return (
+    <div className="col-md-12" id="barNav">
+                <nav className="navbar navbar-blue">
+              <ul className="nav navbar-nav">
+                <li className="active menuDesktop" onClick="newTab(0)"><a href="#" className="currentTab==0 ? linkBackColor: ''">Words</a></li>
+                <li className="menuDesktop"><a href="#" onClick="newTab(1)" className="currentTab==1 ? linkBackColor: ''">Technologies</a></li>
+                <li className="menuDesktop"><a href="#" onClick="newTab(2)" className="currentTab==2 ? linkBackColor: ''">Tests</a></li>
+                <li className="menuDesktop"><a href="#" onClick="exit()">Exit</a></li>
+                <li className="menuSmarthPhone" >
+                    <a className="nav-link dropdown-toggle fontSizeMenu" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">menu
+        </a>
+        <div className="dropdown-menu dropDownColor" aria-labelledby="navbarDropdownMenuLink">
+          <ul className="dropDownUlFont">
+              <li onClick="newTab(0)"><a href="#" className="currentTab==0 ? linkBackColor: ''">Words</a></li>
+                <li><a href="#" onClick="newTab(1)" className="currentTab==1 ? linkBackColor: ''">Technologies</a></li>
+                <li><a href="#" onClick="exit()">Exit</a></li>
+          </ul>
+        </div>
+                </li>
+              </ul>
+          </nav>
+            </div>
+  )
+}
+
+export default Navbar;
