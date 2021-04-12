0911da1685e93649f22595152f0ee6c0f4834301
oscarporras02 <oscarrussi@outlook.com>
2021-04-12 17:58:06 -0500

    add item to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ function Navbar() {
                 <li className="active menuDesktop" onClick="newTab(0)"><a href="#" className="currentTab==0 ? linkBackColor: ''">Words</a></li>
                 <li className="menuDesktop"><a href="#" onClick="newTab(1)" className="currentTab==1 ? linkBackColor: ''">Technologies</a></li>
                 <li className="menuDesktop"><a href="#" onClick="newTab(2)" className="currentTab==2 ? linkBackColor: ''">Tests</a></li>
+                <li className="menuDesktop"><a href="#" onClick="newTab(3)" className="currentTab==2 ? linkBackColor: ''">History</a></li>
                 <li className="menuDesktop"><a href="#" onClick="exit()">Exit</a></li>
                 <li className="menuSmarthPhone" >
                     <a className="nav-link dropdown-toggle fontSizeMenu" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">menu
