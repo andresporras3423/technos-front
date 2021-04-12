f3c1c0b8adcf7b9ad914a76f467bb7ac13e82d5a
oscarporras02 <oscarrussi@outlook.com>
2021-04-11 22:21:57 -0500

    add form to login and signup

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,15 @@
 function Login(props) {
     const {message} = props;
   return (
-    <div>
-     {message}
+    <div className="col-md-12 login-div">
+                <div  className="form-login" >
+                    <label>email</label>
+                    <input  class="form-control" type="text"/>
+                    <label>password</label>
+                    <input className="form-control" type="password"/>
+                </div>      
+                <div><button className="form-control">login</button></div>
+                <div>{message}</div>
     </div>
   );
 }
