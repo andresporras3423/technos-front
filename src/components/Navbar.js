97268e772ce5dda8341a42a32e3593218ef9c50f
oscarporras02 <oscarrussi@outlook.com>
2021-04-13 16:51:00 -0500

    add style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,12 @@
 function Navbar() {
   return (
-    <div className="col-md-12" id="barNav">
-                <nav className="navbar navbar-blue">
-              <ul className="nav navbar-nav ul-inline">
-                <li className="active menuDesktop" onClick="newTab(0)"><a href="#" className="currentTab==0 ? linkBackColor: ''">Words</a></li>
-                <li className="menuDesktop"><a href="#" onClick="newTab(1)" className="currentTab==1 ? linkBackColor: ''">Technologies</a></li>
-                <li className="menuDesktop"><a href="#" onClick="newTab(2)" className="currentTab==2 ? linkBackColor: ''">Tests</a></li>
-                <li className="menuDesktop"><a href="#" onClick="newTab(3)" className="currentTab==2 ? linkBackColor: ''">History</a></li>
-                <li className="menuDesktop"><a href="#" onClick="exit()">Exit</a></li>
-                <li className="menuSmarthPhone" >
-                    <a className="nav-link dropdown-toggle fontSizeMenu" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">menu
-        </a>
-        <div className="dropdown-menu dropDownColor" aria-labelledby="navbarDropdownMenuLink">
-          <ul className="dropDownUlFont">
-              <li onClick="newTab(0)"><a href="#" className="currentTab==0 ? linkBackColor: ''">Words</a></li>
-                <li><a href="#" onClick="newTab(1)" className="currentTab==1 ? linkBackColor: ''">Technologies</a></li>
-                <li><a href="#" onClick="exit()">Exit</a></li>
-          </ul>
-        </div>
-                </li>
-              </ul>
-          </nav>
-            </div>
+    <nav class="links-container">
+      <span>Words</span>
+      <span>Technologies</span>
+      <span>Tests</span>
+      <span>History</span>
+      <span>Exit</span>
+    </nav>
   )
 }
 
