ba80e65d28ca02413bda4c85be5b414b5c29ac26
oscarporras02 <oscarrussi@outlook.com>
2021-04-13 17:08:55 -0500

    add navbar for small screen size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 function Navbar() {
   return (
-    <nav class="links-container">
+    <div>
+      <nav class="links-container">
       <span>Words</span>
       <span>Technologies</span>
       <span>Tests</span>
       <span>History</span>
       <span>Exit</span>
     </nav>
+    <select class="nav-select">
+    <option>Menu</option>
+      <option>Words</option>
+      <option>Technologies</option>
+      <option>Tests</option>
+      <option>History</option>
+      <option>Exit</option>
+    </select>
+    </div>
   )
 }
 
