bfcc51c978169eca2454db10bc9b9971d83370d0
oscarporras02 <oscarrussi@outlook.com>
2021-04-15 17:57:51 -0500

    add form for add words

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,12 @@
 import Navbar from './Navbar';
+import Words from './Words';
 
 function Dashboard() {
+  const selected=0;
   return (
     <div>
      <Navbar/>
+     {selected==0 && <Words/>}
     </div>
   );
 }
---
+++ src/components/Words.js
@@ -0,0 +1,21 @@
+function Words() {
+    return (
+      <div class="wordDiv">
+          <div class="formContainer">
+              <label>Technology</label>
+              <select>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+              </select>
+              <label>Word</label>
+              <input></input>
+        </div>
+        <div>
+            hello
+        </div>
+      </div>
+    )
+  }
+  
+  export default Words;
