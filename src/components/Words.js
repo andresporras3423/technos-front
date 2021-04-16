4032997ea48edf3a120891a1e2357921f369aefc
oscarporras02 <oscarrussi@outlook.com>
2021-04-15 19:12:39 -0500

    styling form for add words

--- src/components/Words.js
+++ src/components/Words.js
@@ -2,14 +2,28 @@ function Words() {
     return (
       <div class="wordDiv">
           <div class="formContainer">
-              <label>Technology</label>
+              <label><strong>Technology</strong></label>
               <select>
                   <option value="0">0</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
               </select>
-              <label>Word</label>
+              <label><strong>Word</strong></label>
               <input></input>
+              <label><strong>Meaning</strong></label>
+              <textarea cols="20" rows="3"></textarea>
+              <div class="twoButtons">
+                  <button className="btn btn-primary">Save</button>
+                  <button className="btn btn-primary">clear</button>
+              </div>
+              <div class="twoButtons">
+                  <button className="btn btn-primary">Search</button>
+                  <select>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+              </select>
+              </div>
         </div>
         <div>
             hello
