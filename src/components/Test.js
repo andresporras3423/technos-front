f957077e44cd1e185379a4ffaaa2b82c56602b30
oscarporras02 <oscarrussi@outlook.com>
2021-04-18 18:38:44 -0500

    add styling and content to tests component

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,6 +2,13 @@ import {useState} from 'react';
 function Test() {
     const [questions, setQuestions] = useState(10);
 
+    const listQuestions = [
+        "question 1",
+        "question 2",
+        "question 3",
+        "question 4",
+    ];
+
     const callUpdateNQuestions = (e)=>{
         setQuestions(e.target.value);
     };
@@ -21,17 +28,24 @@ function Test() {
                   <input type="number" className="w-100" min="1" value={questions} onChange={callUpdateNQuestions}/>
               </div>
           </div>
-          <h2>
-
-          </h2>
-          <h2>
-
-          </h2>
+          <h4>
+          TECHNOLOGY: sql queries
+          </h4>
+          <h4>
+          WORD: Contest Leaderboard
+          </h4>
           <div>
-
+              {listQuestions.map((item, index)=>(
+                      <div className="itemTest">
+                          <input name="opts" type="radio" value={index} />
+                          <textarea row="5" col="75" disabled>{item}</textarea>
+                      </div>
+                  )
+              )}
           </div>
-          <div>
-
+          <div className="topTest">
+              <h5 className="w-100">2/10</h5>
+              <button className="w-100 btn btn-dark">next</button>
           </div>
       </div>
     )
