1b86a3b9915537d09ab7ebd7d1ce74d72527f500
oscarporras02 <oscarrussi@outlook.com>
2021-04-19 19:38:04 -0500

    add content and styling to  history component

--- src/components/History.js
+++ src/components/History.js
@@ -1,52 +1,72 @@
-import {useState} from 'react';
+import Test from "./Test";
+
 function History() {
-    const [questions, setQuestions] = useState(10);
-
-    const listQuestions = [
-        "question 1",
-        "question 2",
-        "question 3",
-        "question 4",
+    const listTests = [
+        {"date": "12/31/2020", "correct": "5", "total": "10"},
+        {"date": "12/31/2020", "correct": "2", "total": "10"},
+        {"date": "12/31/2020", "correct": "6", "total": "10"},
+        {"date": "12/31/2020", "correct": "7", "total": "10"},
+        {"date": "12/31/2020", "correct": "8", "total": "10"},
+        {"date": "12/31/2020", "correct": "5", "total": "10"},
+        {"date": "12/31/2020", "correct": "5", "total": "10"},
+        {"date": "12/31/2020", "correct": "5", "total": "10"},
+        {"date": "12/31/2020", "correct": "9", "total": "10"},
+        {"date": "12/31/2020", "correct": "3", "total": "10"},
     ];
-
-    const callUpdateNQuestions = (e)=>{
-        setQuestions(e.target.value);
-    };
     return (
       <div className="testContainer">
+          <div className="tableContainer">
           <div className="topTest">
-              <div>
-                  <h4>Technology:</h4>
-                  <select className="w-100">
-                  <option value="0">0</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-              </select>
-              </div>
-              <div>
-                  <h4>How many?:</h4>
-                  <input type="number" className="w-100" min="1" value={questions} onChange={callUpdateNQuestions}/>
-              </div>
+              <h5>
+              GLOBAL SCORE:
+              </h5>
+              <h5 style={{backgroundColor: 17/30 <= 0.5 ? `rgb(255,${parseInt(255*(17/30))},0)`: `rgb(${parseInt(255-(255*(17/30)))}, 255,0)`}}>
+              17 / 30 ( 56.67 %)
+              </h5>
           </div>
-          <h4>
-          TECHNOLOGY: sql queries
-          </h4>
-          <h4>
-          WORD: Contest Leaderboard
-          </h4>
-          <div>
-              {listQuestions.map((item, index)=>(
-                      <div className="itemTest">
-                          <input name="opts" type="radio" value={index} />
-                          <textarea row="5" col="75" disabled>{item}</textarea>
-                      </div>
-                  )
-              )}
-          </div>
-          <div className="topTest">
-              <h5 className="w-100">2/10</h5>
-              <button className="w-100 btn btn-dark">next</button>
-          </div>
+            <div>
+                <table className="table tableContent">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>Date</th>
+                            <th>Correct Questions</th>
+                            <th>Total Questions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            listTests.map(
+                                (test)=>(
+                                    <tr style={{backgroundColor: test.correct/test.total <= 0.5 ? `rgb(255,${parseInt(255*(test.correct/test.total))},0)`: `rgb(${parseInt(255-(255*(test.correct/test.total)))}, 255,0)`}}>
+                                        <td>{test.date}</td>
+                                        <td>{test.correct}</td>
+                                        <td>{test.total}</td>
+                                    </tr>
+                                )
+                            )
+                        }
+                    </tbody>
+                </table>
+                <div className="fiveColumns">
+                    <button className="btn btn-dark">
+                        <i className="fas fa-fast-backward"></i>
+                        </button>
+                        <button className="btn btn-dark">
+                        <i className="fas fa-step-backward"></i>
+                        </button>
+                        <span>
+                            <div>6/10</div>
+                            <div>10 tests</div>
+                            </span>
+                        <button className="btn btn-dark">
+                        <i className="fas fa-step-forward"></i>
+                        </button>
+                        <button className="btn btn-dark">
+                        <i className="fas fa-fast-forward"></i>
+                        </button>
+                    </div>
+            </div>
+        </div>
       </div>
     )
   }
