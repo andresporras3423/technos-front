c2366466c6ef2d4721bce73d136284011011cfd4
oscarporras02 <oscarrussi@outlook.com>
2021-05-10 22:04:52 -0500

    add methods for techno endpoints

--- src/data/technoData.js
+++ src/data/technoData.js
@@ -1,12 +1,64 @@
-export const createTechno = async (email, password) => {
+export const createTechno = async (techno_name, techno_status) => {
     const response = await fetch(`https://hidden-plateau-07048.herokuapp.com/techno/create`, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({email: email, password: password})
+        body: JSON.stringify({techno_name: techno_name, techno_status: techno_status})
       });
       const data = await response;
       return data;
 };
+
+export const updateTechno = async (id, techno_name, techno_status) => {
+    const response = await fetch(`https://hidden-plateau-07048.herokuapp.com/techno/update`, {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({id: id, techno_name: techno_name, techno_status: techno_status})
+      });
+      const data = await response;
+      return data;
+};
+
+export const getTechno = async (sort_by_name) => {
+    const response = await fetch(`https://hidden-plateau-07048.herokuapp.com/techno/get?sort_by_name=${sort_by_name}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      });
+      const data = await response;
+      return data;
+};
+
+export const searchTechno = async (sort_by_name, search, techno_name, techno_status) => {
+    const params = ( new URLSearchParams( {sort_by_name: sort_by_name, search: search, techno_name: techno_name, techno_status: techno_status} ) ).toString();
+    const response = await fetch(`https://hidden-plateau-07048.herokuapp.com/techno/search?${params}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      });
+      const data = await response;
+      return data;
+};
+
+export const deleteTechno = async (id) => {
+    const response = await fetch(`https://hidden-plateau-07048.herokuapp.com/techno/delete?id=${id}`, {
+        method: 'DELETE',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      });
+      const data = await response;
+      return data;
+  };
+
+  export default {createTechno, updateTechno, getTechno, searchTechno, deleteTechno}
